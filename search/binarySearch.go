057f750607fd6bc97d9e737800cf2a8e13b51bb4
o-richard <antoniodiazway@protonmail.com>
2023-10-13 18:50:10 +0300

    Improved binary search

--- search/binarySearch.go
+++ search/binarySearch.go
@@ -1,40 +1,12 @@
 package search
 
-func maxHeapify(nums []int, itemsCount, largestIndex int) {
-	largest := largestIndex
-	left := 2 * largestIndex + 1
-	right := 2 * largestIndex + 2
-
-	if left < itemsCount && nums[largestIndex] < nums[left] {
-		largest = left
-	}
-
-	if right < itemsCount && nums[largest] < nums[right] {
-		largest = right
-	}
-
-	if largest != largestIndex {
-		nums[largestIndex], nums[largest] = nums[largest], nums[largestIndex]
-		maxHeapify(nums, itemsCount, largest)
-	}
-}
-
-func heapSort(nums []int) ([]int, int) {
-	itemsCount := len(nums)
-	for i := (itemsCount / 2); i > -1; i-- {
-		maxHeapify(nums, itemsCount, i)
-	}
-	for i := itemsCount - 1; i >= 0; i-- {
-		nums[i], nums[0] = nums[0], nums[i]
-		maxHeapify(nums, i, 0)
-	}
-	return nums, itemsCount
-} 
+import "golang.org/x/exp/slices"
 
 func BinarySearch(nums []int, searchData int) (int, bool) {
 	var isFound bool
 	var index int
-	nums, itemsCount := heapSort(nums)
+	itemsCount := len(nums)
+	slices.Sort(nums)
 	for start, end := 0, itemsCount - 1 ; start <= end; {
 		mid := (start + end) / 2
 
